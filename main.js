df2312b53a0bf7b981d2ef0eef2d7026d440ea0c
Luckkk1 <dldbstjr4444@gmail.com>
2022-05-29 22:57:18 +0900

    JS:quit content box when click quitBtn

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-// content box  미니 버튼 클릭 시 최소화
+"use strict";
+
 const miniBtns = document.querySelectorAll(".miniBtn");
+const quitBtns = document.querySelectorAll(".quitBtn");
 const boxes = document.querySelectorAll(".box");
 
+// content box  미니 버튼 클릭 시 최소화
 miniBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    for (box of boxes) {
+    for (let box of boxes) {
       if (btn.dataset.key === box.dataset.value) {
         box.style.height = "25em";
         box.style.width = "25em";
+      }
+    }
+  });
+});
+
+// content box 나가기 버튼 클릭 시 삭제
+quitBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    for (let box of boxes) {
+      if (btn.dataset.key === box.dataset.value) {
+        box.style.display = "none";
       }
     }
   });
