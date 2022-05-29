ba057c5c06cffe968890b51dad37786728e6f5f1
Luckkk1 <dldbstjr4444@gmail.com>
2022-05-29 22:44:40 +0900

    JS: minimalize content box when click minibtn

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+// content box  미니 버튼 클릭 시 최소화
+const miniBtns = document.querySelectorAll(".miniBtn");
+const boxes = document.querySelectorAll(".box");
 
+miniBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    for (box of boxes) {
+      if (btn.dataset.key === box.dataset.value) {
+        box.style.height = "25em";
+        box.style.width = "25em";
+      }
+    }
+  });
+});
