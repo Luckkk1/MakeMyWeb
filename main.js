958b6861e6b0fbb46721278263e88d67ef968650
Luckkk1 <dldbstjr4444@gmail.com>
2022-06-02 23:24:33 +0900

    JS: Drag And Drop, Resize

--- main.js
+++ main.js
@@ -427,3 +427,45 @@ rgbInput.addEventListener("change", (e) => {
     }
   });
 });
+
+// <Drag And Drop, Resize>
+const contentBoxes = document.querySelectorAll(".box");
+const position = { x: 0, y: 0 };
+
+for (let box of contentBoxes) {
+  interact(box).draggable({
+    listeners: {
+      move: dragMoveListener,
+    },
+  });
+  function dragMoveListener(event) {
+    var target = event.target;
+    // keep the dragged position in the data-x/data-y attributes
+    var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+    var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+    // translate the element
+    target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+    // update the posiion attributes
+    target.setAttribute("data-x", x);
+    target.setAttribute("data-y", y);
+  }
+
+  interact(box).resizable({
+    edges: { top: true, left: true, bottom: true, right: true },
+    listeners: {
+      move: function (event) {
+        let { x, y } = event.target.dataset;
+        x = (parseFloat(x) || 0) + event.deltaRect.left;
+        y = (parseFloat(y) || 0) + event.deltaRect.top;
+        Object.assign(event.target.style, {
+          width: `${event.rect.width}px`,
+          height: `${event.rect.height}px`,
+          transform: `translate(${x}px, ${y}px)`,
+        });
+        Object.assign(event.target.dataset, { x, y });
+      },
+    },
+  });
+}
